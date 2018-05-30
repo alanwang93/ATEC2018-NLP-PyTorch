c425660da5c4750f6d270a673edec7280a67e68f
Valentine233 <494543506@qq.com>
2018-05-30 08:59:01 +0800

    add ainn model

--- models/ainn.py
+++ models/ainn.py
@@ -7,22 +7,17 @@ import numpy as np
 UNK_IDX = 0
 EOS_IDX = 2
 
-class MatchPyramid(nn.Module):
+class AINN(nn.Module):
     def __init__(self, config, data_config):
-        super(MatchPyramid, self).__init__()
+        super(AINN, self).__init__()
         self.char_size = data_config['char_size']
         self.embed_size = config['embed_size']
         self.config = config
         self.data_config = data_config
 
         self.embed = nn.Embedding(self.char_size, self.embed_size, padding_idx=EOS_IDX)
-        self.conv1 = nn.Conv2d(1, 16, 5, padding=2)
-        self.conv2 = nn.Conv2d(16, 32, 3, padding=2)
+        self.conv_separate = nn.Conv2d(1, 1, 3)
         self.relu = nn.ReLU()
-        self.maxpool1 = nn.MaxPool2d(2)
-        self.maxpool2 = nn.AdaptiveMaxPool2d(output_size=5)
-        self.fc1 = nn.Linear(5*5*32, 10)
-        self.fc2 = nn.Linear(10, 1)
         self.sigmoid = nn.Sigmoid()
         self.dropout = nn.Dropout(config['dropout'])
 
@@ -35,25 +30,52 @@ class MatchPyramid(nn.Module):
     def _init_weights(self):
         pass
 
+
     def forward(self, data):
         batch_size = data['s1_char'].size()[0]
         embed_1 = self.embed(data['s1_char'])
         embed_2 = self.embed(data['s2_char'])
-        embed_1 = self.dropout(embed_1)
-        embed_2 = self.dropout(embed_2)
-        # dot matching
-        embed_2 = torch.transpose(embed_2, 1, 2)
-        matching = torch.bmm(embed_1, embed_2).unsqueeze(1)
-        print("embded size", matching.size())
-        output = self.conv1(matching)
-        output = self.relu(output)
-        output = self.maxpool1(output)
-        output = self.relu(self.conv2(output))
-        output = self.maxpool2(output)
-        output = output.view((batch_size, -1))
-        output = self.fc1(output)
-        output = self.dropout(output)
-        output = self.fc2(output)
+        embed_1 = self.dropout(embed_1).unsqueeze(1)
+        embed_2 = self.dropout(embed_2).unsqueeze(1)
+
+        # print("h1_pre",embed_1.size())
+        # print("h2_pre",embed_2.size())
+
+        h1 = self.relu(self.conv_separate(embed_1)).squeeze(1)
+        h2 = self.relu(self.conv_separate(embed_2)).squeeze(1)
+
+        self.conv_together = nn.Conv2d(h1.size()[-1], h1.size()[-1], (1, 2), stride=(1, 2))
+
+        # print("h1",h1.size())
+        # print("h2",h2.size())
+
+        h1_t = h1.unsqueeze(2)
+        h2_t = h2.unsqueeze(1).repeat(1, h1_t.size()[1], 1, 1)
+
+        c = torch.FloatTensor([])
+        for i in range(h2_t.size()[2]):
+            c = torch.cat((c, h1_t, h2_t[:, :, i].unsqueeze(2)), 2)
+        c = torch.transpose(c, 1, 2)
+        c = torch.transpose(c, 1, 3)
+        # print("c", c.size())
+
+        A = self.relu(self.conv_together(c))
+        # print("A", A.size())
+        r1, _ = torch.max(A, 3)
+        r2, _ = torch.max(A, 2)
+
+        r1 = torch.transpose(r1, 1, 2)
+        r2 = torch.transpose(r2, 1, 2)
+
+        # print("h1",h1.size())
+        # print("r1",r1.size())
+        # print("h2",h2.size())
+        # print("r2",r2.size())
+
+        R1 = (h1 * r1).view(-1)
+        R2 = (h2 * r2).view(-1)
+
+        output = torch.dot(R1, R2)
         return output
 
     def load_vectors(self, char=None, word=None):
@@ -62,7 +84,7 @@ class MatchPyramid(nn.Module):
             self.embed.weight = nn.Parameter(torch.FloatTensor(char))
 
     def train_step(self, data):
-        proba = self.sigmoid(self.forward(data)).squeeze(1)
+        proba = self.sigmoid(self.forward(data))
         target = data['target']
         loss = self.criterion(proba, target, weights=[1.0, 3.0])
         self.optimizer.zero_grad()
@@ -70,8 +92,9 @@ class MatchPyramid(nn.Module):
         self.optimizer.step()
         return loss.item()
 
+
     def evaluate(self, data):
-        proba = self.sigmoid(self.forward(data)).squeeze(1)
+        proba = self.sigmoid(self.forward(data))
         target =  data['target']
         loss = self.criterion(proba, target, weights=[1.0, 3.0])
         return proba.item(), target.item(), loss.item()
